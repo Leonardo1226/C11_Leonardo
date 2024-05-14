3c7a0087c540a65764d694449579d363fb6e1722
Leonardo1226 <leonardocardenas26122008@gmail.com>
2024-05-13 20:12:15 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+var trex ,trex_running;
+var ground;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite de Trex
+  trex = createSprite(50, 180, 20, 50);
+  trex.addAnimation("runing", trex_running);
+
+  //agregar tamaño y posicion al trex
+  trex.scale = 0.5;
+
+  //crear sprite del suelo
+  ground = createSprite(200, 180, 400, 20);
+
+}
+
+function draw(){
+  background("white")
+  
+  if(keyDown("space")){
+    trex.velocityY = -10;  
+  }
+  
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  //trex choca con el suelo
+  trex.collide(ground);
+  
+  
+  
+  drawSprites();
+}
